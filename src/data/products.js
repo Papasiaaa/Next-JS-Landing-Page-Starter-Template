474273e615e49ccda3aa464870d23eb808200af0
Papasiaaa <ruzegari52@gmail.com>
2025-09-26 14:19:21 +0300

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,19 @@
+// src/data/products.js
+export const products = [
+  {
+    id: "1",
+    title: "Sample eBook",
+    description: "توضیح کوتاه دربارهٔ کتاب. پیش‌نمایش 5 صفحه.",
+    priceText: "10 USDT",
+    price: 10,
+    image: "/assets/images/book-sample.jpg"
+  },
+  {
+    id: "2",
+    title: "Another eBook",
+    description: "توضیح کوتاه برای کتاب دوم.",
+    priceText: "8 USDT",
+    price: 8,
+    image: "/assets/images/book2.jpg"
+  }
+];
